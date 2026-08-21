2fd9431e7755b01b4bfff8e976840361851212a7
dugongbekasi <akbarirfanp008@gmail.com>
2026-08-21 08:35:41 +0700

    update v1.00 add models, services, migration, controller, dbcontext example, and db setting env

---
+++ StockFlow/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using StockFlow.Models;
+using Microsoft.AspNetCore.Mvc;
+using StockFlow.Services;
+using System.Reflection.PortableExecutable;
+
+namespace StockFlow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(IUserService service) : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetUsers() => Ok(await service.GetAllUsersAsync());
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(Guid id)
+        {
+            var user = await service.GetUserByIdAsync(id);
+            if (user is null)
+            {
+                return NotFound("User with the given id was not found");
+            }
+            return Ok(user);
+        }
+    }
+}
---
+++ StockFlow/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿namespace StockFlow.Services;
+
+using StockFlow.Models;
+
+public interface IUserService
+{
+    Task<List<User>> GetAllUsersAsync();
+    // ? means could be null
+    Task<User?> GetUserByIdAsync(Guid id);
+    Task<User> CreateUserAsync(User user);
+    Task<bool> UpdateUserAsync(Guid id, User user);
+    Task<bool> DeleteUserAsync(Guid id);
+}
